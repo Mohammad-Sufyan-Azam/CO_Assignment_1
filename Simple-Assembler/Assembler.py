ec0879afe86d5e65fec6f532c1248dd1e6d8975a
Shourya06 <88823526+Shourya06@users.noreply.github.com>
2021-08-15 02:10:57 +0530

    Update Assembler.py

--- Simple-Assembler/Assembler.py
+++ Simple-Assembler/Assembler.py
@@ -1,58 +1,67 @@
 from sys import stdin
+
 # import Checkerror
 
-opcode = { "add": "00000", "sub": "00001", "ld": "00100", "st": "00101", "mul": "00110", "div": "00111",
-           "rs": "01000", "ls": "01001", "xor": "01010", "or": "01011", "and": "01100", "not": "01101",
-           "cmp": "01110", "jmp": "01111", "jlt": "10000", "jgt": "10001", "je": "10010", "hlt": "10011" }
+opcode = {"add": "00000", "sub": "00001", "ld": "00100", "st": "00101", "mul": "00110", "div": "00111",
+          "rs": "01000", "ls": "01001", "xor": "01010", "or": "01011", "and": "01100", "not": "01101",
+          "cmp": "01110", "jmp": "01111", "jlt": "10000", "jgt": "10001", "je": "10010", "hlt": "10011"}
 
 reg = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100", "R5": "101", "R6": "110", "FLAGS": "111"}
 # mov operation handled separately
 # FLAGS not given in any case
 # case where any other instruction can call labels not handled
 # registers not initialised to zero
-typeA = ("add","sub","mul", "xor", "or", "and")
+typeA = ("add", "sub", "mul", "xor", "or", "and")
 typeB = ("rs", "ls")
 typeC = ("div", "not", "cmp")
 typeD = ("ld", "st")
 typeE = ("jmp", "jlt", "jgt", "je")
 
+
 def mov(arr):
     if arr[2][0] == "R":
-        print( '00011' + '00000' + reg[arr[1]] + reg[arr[2]] )
+        print('00011' + '00000' + reg[arr[1]] + reg[arr[2]])
 
     elif arr[2][0] == "$":
-        imm = str( bin( int(arr[2][1:]) ) ).replace("0b", "")
+        imm = str(bin(int(arr[2][1:]))).replace("0b", "")
         imm = '0' * (8 - len(imm)) + imm
-        print( '00010' + reg[arr[1]] + imm )
+        print('00010' + reg[arr[1]] + imm)
+
 
 def type_A(arr):
     print(opcode[arr[0]] + '00' + reg[arr[1]] + reg[arr[2]] + reg[arr[3]])
 
+
 def type_B(arr):
-    imm = str( bin( int(arr[2][1:]) ) ).replace("0b", "")
+    imm = str(bin(int(arr[2][1:]))).replace("0b", "")
     imm = '0' * (8 - len(imm)) + imm
     print(opcode[arr[0]] + reg[arr[1]] + imm)
+
 
 def type_C(arr):
     print(opcode[arr[0]] + '00000' + reg[arr[1]] + reg[arr[2]])
 
-def type_D(arr):
-    # insert memory address also
-    print(opcode[arr[0]] + reg[arr[1]] )
 
-def type_E(arr):
+def type_D(arr, mem):
     # insert memory address also
-    print(opcode[arr[0]] + '000' )
+    print(opcode[arr[0]] + reg[arr[1]] + mem)
+
+
+def type_E(arr, mem):
+    # insert memory address also
+    print(opcode[arr[0]] + '000' + mem)
+
 
 def type_F(arr):
     print(opcode[arr[0]] + '00000000000')
+
 
 def main():
     user_input = []
     count = 1
     for line in stdin:
         if count == 257:
-           break
+            break
         user_input.append(line)
     count = 0
     for line in user_input[-1::]:
@@ -61,10 +70,28 @@ def main():
         count += 1
     user_input = user_input[0:len(user_input) - count]
 
+    memory_add = {}
+    count_var = 0
+    for i in range(len(user_input)):
+        if user_input[i][0:3] == "var":
+            count_var += 1
+        elif user_input[i][0:3] != "var":
+            break
+    count = 0
+    for i in range(len(user_input)):
+        if user_input[i][0:3] == "var":
+            memory_add[user_input[i][4::]] = str(bin(len(user_input) - count_var)).replace('0b', '')
+            count_var += 1
+            continue
+        l = [x for x in user_input[i].split()]
+        if l[0][-1] == ":":
+            memory_add[l[0][:-1]] = str(bin(count)).replace('0b', '')
+        count += 1
+
     # res = Checkerrror.check(user_input)
     # if res == False:      # False means no error is present
     for i in user_input:
-        a = [ x for x in i.split() ]
+        a = [x for x in i.split()]
         if a[0] == 'label:':
             a = a[1:]
         if a[0] in typeA:
@@ -74,9 +101,9 @@ def main():
         elif a[0] in typeC:
             type_C(a)
         elif a[0] in typeD:
-            type_D(a)
+            type_D(a, memory_add[a[2]])
         elif a[0] in typeE:
-            type_E(a)
+            type_E(a, a[1])
         elif a[0] == 'hlt':
             type_F(a)
         elif a[0] == 'mov':
@@ -84,3 +111,4 @@ def main():
 
 
 main()
+
